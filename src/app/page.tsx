8bc18a5b00f41f7b42894c7aed0d7dd23debc211
Ashkan Hajmohammadi <Ashkan.H.Mohamadi@gmail.com>
2025-08-12 21:45:00 +0000

    Reposition the logo and move it to the upper right side of the screen, i

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -284,8 +284,8 @@ const WhisperWriterPage: NextPage = () => {
                 <Languages className="h-5 w-5" />
                 <span>More Languages</span>
             </button>
-          <div className="relative z-10 flex flex-col items-center text-center -mt-2">
-            <img src="/Images/ww_logo.webp" alt="Whisper Writer Logo" className="h-[47px] w-auto" />
+           <div className="absolute top-4 right-4 z-10">
+            <img src="/Images/ww_logo.webp" alt="Whisper Writer Logo" className="h-[52px] w-auto" />
           </div>
           {/* Recording Controls - Moved into Header */}
           <div className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 flex items-center justify-center space-x-4 z-20">
@@ -427,3 +427,5 @@ const WhisperWriterPage: NextPage = () => {
 };
 
 export default WhisperWriterPage;
+
+    
