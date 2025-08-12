93c833f19aa5000274c16d7c87bbb05d0d55123e
Ashkan Hajmohammadi <Ashkan.H.Mohamadi@gmail.com>
2025-08-12 17:02:24 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,7 +80,7 @@ const WhisperWriterPage: NextPage = () => {
     setSelectedLanguage(DEFAULT_LANGUAGE);
   }, []);
 
- const handleCancelRecording = () => {
+  const handleCancelRecording = () => {
     if (mediaRecorderRef.current) {
         isCancelledRef.current = true;
         // Directly stop tracks to avoid onstop event firing with valid data
@@ -241,11 +241,8 @@ const WhisperWriterPage: NextPage = () => {
   };
   
   const getSelectedLanguageLabel = () => {
-    if (!['en-US', 'fa-IR'].includes(selectedLanguage)) {
-        const selectedLang = LANGUAGES.find(lang => lang.value === selectedLanguage);
-        return selectedLang ? selectedLang.label : 'More Languages';
-    }
-    return 'More Languages';
+      const selectedLang = LANGUAGES.find(lang => lang.value === selectedLanguage);
+      return selectedLang ? selectedLang.label : 'More Languages';
   };
 
   const moreLanguagesButtonLabel = getSelectedLanguageLabel();
@@ -265,7 +262,8 @@ const WhisperWriterPage: NextPage = () => {
               {showCustomLanguage ? <span className="text-sm">{moreLanguagesButtonLabel}</span> : <Languages className="h-6 w-6" />}
           </Button>
           <div className="relative z-10 flex flex-col items-center text-center">
-            <img src="/icons/app-icon.svg" alt="Whisper Writer Logo" className="h-16 w-16 mb-2" />
+            <img src="/icons/WW logo.webp" alt="Whisper Writer Logo" className="h-12 w-auto" />
+            <img src="/icons/WW logo type.webp" alt="Whisper Writer Logotype" className="h-8 w-auto mt-2" />
           </div>
           {/* Recording Controls - Moved into Header */}
           <div className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 flex items-center justify-center space-x-4">
@@ -396,3 +394,5 @@ const WhisperWriterPage: NextPage = () => {
 };
 
 export default WhisperWriterPage;
+
+    
